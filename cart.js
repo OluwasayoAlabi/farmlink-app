92e24c85a257bb0de81916fa14fc5e48747c2086
Reny-Faar <ifaaradong23@st.ubids.edu.gh>
2025-06-08 22:15:21 +0000

    added navbar and corrected currency

--- cart.js
+++ cart.js
@@ -85,12 +85,14 @@ function addItemToCart(title, price, image) {
   `;
 
   cartItemsContainer.appendChild(cartRow);
+  saveCartToLocalStorage();
 }
 
 function removeCartItem(event) {
   const buttonClicked = event.target;
   buttonClicked.parentElement.parentElement.remove();
   updateTotal();
+  saveCartToLocalStorage();
 }
 
 function quantityChanged(event) {
@@ -99,6 +101,31 @@ function quantityChanged(event) {
     input.value = 1;
   }
   updateTotal();
+  saveCartToLocalStorage();
+}
+
+function saveCartToLocalStorage() {
+  const items = [];
+  const cartRows = document.querySelectorAll(".cart-row");
+
+  cartRows.forEach((row) => {
+    const title = row.querySelector(".cart-title").innerText;
+    const price = row.querySelector(".cart-price").innerText;
+    const image = row.querySelector("img").src;
+    const quantity = row.querySelector(".cart-gty").value;
+
+    items.push({ title, price, image, quantity });
+  });
+
+  localStorage.setItem("cartItems", JSON.stringify(items));
+}
+
+function loadCartFromLocalStorage() {
+  const savedItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+
+  savedItems.forEach((item) => {
+    addItemToCart(item.title, item.price, item.image, item.quantity);
+  });
 }
 
 function updateTotal() {
@@ -116,7 +143,7 @@ function updateTotal() {
 
   total = Math.round(total * 100) / 100;
 
-  document.getElementsByClassName("total")[0].innerText = `$${total.toFixed(
+  document.getElementsByClassName("total")[0].innerText = `₦${total.toFixed(
     2
   )}`;
 }
@@ -147,7 +174,7 @@ function showAddToCartMessage(button) {
 
   button.innerHTML = "✓ Item Added";
   button.className =
-    "add-to-cart bg-[#FF6600] text-white font-semibold px-4 py-2 rounded transition";
+    "add-to-cart bg-green-400 text-white font-semibold px-4 py-2 rounded transition";
 
   setTimeout(() => {
     button.innerHTML = originalText;
@@ -193,7 +220,7 @@ const categories = [
         id: 5,
         name: "Garden Eggs",
         price: 3000,
-        quantity: "1 dozen",
+        unit: "1 dozen",
         image: "images/garden eggs.jpg",
       },
       {
@@ -208,14 +235,14 @@ const categories = [
         id: 5,
         name: "Plaintains",
         price: 2000,
-        quantity: "1 bunch",
+        unit: "1 bunch",
         image: "images/plaintains.webp",
       },
       {
         id: 5,
         name: "Cucumbers",
         price: 1500,
-        quantity: "1pc",
+        unit: "1pc",
         image: "images/cucumber 2.webp",
       },
     ],
@@ -563,4 +590,9 @@ function setupPaginationControls() {
 
 window.addEventListener("DOMContentLoaded", () => {
   setTimeout(setupPaginationControls, 100);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadCartFromLocalStorage();
+  updateTotal();
 });
